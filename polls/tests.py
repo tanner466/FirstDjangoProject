8c2c902385a190bac766fcd6a2c16fe33d3c661a
tanner466 <tanner466@gmail.com>
2016-04-06 14:19:25 -0400

    Add static files, stylesheets, background image

--- polls/tests.py
+++ polls/tests.py
@@ -18,6 +18,48 @@ def create_question(question_text, days):
                                    pub_date=time)
 
 class QuestionMethodsTests(TestCase):
+    def test_results_view_with_a_future_question(self):
+        """
+        The results view of a question with a pub_date in the future should
+        rturn a 404 not found
+        """
+        future_question = create_question(question_text='Future Question.', days=5)
+        response = self.client.get(reverse('polls:results', args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_results_view_with_a_past_question(self):
+        """
+        The results view of a question with a pub_date in the past should
+        display the results text.
+        """
+        past_question = create_question(question_text='Past Question.',
+                                        days=-5)
+        response = self.client.get(reverse('polls:results',
+                                   args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text,
+                            status_code=200)
+
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should
+        rturn a 404 not found
+        """
+        future_question = create_question(question_text='Future Question.', days=5)
+        response = self.client.get(reverse('polls:detail', args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        display the question's text.
+        """
+        past_question = create_question(question_text='Past Question.',
+                                        days=-5)
+        response = self.client.get(reverse('polls:detail',
+                                   args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text,
+                            status_code=200)
+
     def test_index_view_with_no_questions(self):
         """
         If no questions exist, an appropriate message should be displayed.
@@ -60,8 +102,7 @@ class QuestionMethodsTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
-            ['<Question: Past question>']
-        )
+            ['<Question: Past question>'])
 
     def test_index_view_with_two_past_questions(self):
         """
@@ -83,7 +124,6 @@ class QuestionMethodsTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertEqual(future_question.was_published_recently(), False)
-
 
     def test_was_published_recently_with_old_question(self):
         """
